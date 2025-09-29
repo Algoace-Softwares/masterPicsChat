21c13a55bc4ff0936f553eb5a18a3cc5380552ce
ahmedShaheer3 <122429039+ahmedShaheer3@users.noreply.github.com>
2025-09-29 19:36:33 +0500

    s
    s

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ configDotenv({
 import { httpServer } from "./app";
 import { connectToDatabase } from "./database";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 connectToDatabase()
   .then(() => {
